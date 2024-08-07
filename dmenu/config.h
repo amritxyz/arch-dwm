6ad622a58e7a71cd8cad1f6d0a0c8146609dae3d
amrit-44404 <amrit44404@proton.me>
2024-08-07 18:08:47 +0545

    new color emojis

--- dmenu/config.h
+++ dmenu/config.h
@@ -5,7 +5,7 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"Hack:weight=bold:size=9",
-	"JoyPixels:weight=bold:pixelsize=15:antialias=true:autohint=true"
+	"NotoColorEmoji:weight=bold:pixelsize=10:antialias=true:autohint=true"
 };
 static const unsigned int bgalpha = 0xe0;
 static const unsigned int fgalpha = OPAQUE;
