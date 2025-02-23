485e5bfd8924cfb9b2add26058e82187d3336271
amritxyz <amrit44404@proton.me>
2025-02-23 14:33:22 +0545

    indent to tab

--- slock/config.h
+++ slock/config.h
@@ -3,12 +3,11 @@ static const char *user = "nobody";
 static const char *group = "nobody";
 
 static const char *colorname[NUMCOLS] = {
-    [INIT] = "#0a0a0a",   /* after initialization */
-    [INPUT] = "#005577",  /* during input */
-    [FAILED] = "#770000", /* wrong password */
-    [CAPS] = "#ebdbb2",   /* CapsLock on */
+	[INIT]		=	"#0a0a0a",	/* after initialization */
+	[INPUT]		=	"#005577",	/* during input */
+	[FAILED]	=	"#770000",	/* wrong password */
+	[CAPS]		=	"#ebdbb2",	/* CapsLock on */
 };
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
-
