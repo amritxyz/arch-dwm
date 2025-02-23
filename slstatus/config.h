4e658f06fcfb59bbded6afc2f51e635697234a40
amritxyz <amrit44404@proton.me>
2025-02-23 14:39:47 +0545

    indent to tab

--- slstatus/config.h
+++ slstatus/config.h
@@ -6,18 +6,17 @@ static const char unknown_str[] = "n/a";
 #define MAXLEN 2048
 
 static const struct arg args[] = {
-/* 	function format		argument */
+/* 	function format			argument */
 
-//	{ temp, 		"[TEMP %sC] ",	"/sys/class/thermal/thermal_zone0/temp" },
-//	{ ram_used,		"  %s",	NULL },
-	//{ datetime, 		" %s ",	"%a %b %d %r" },
-//	{ cpu_perc, 	" 🗄️ CPU %s%%  ",NULL},
-//	{ ram_perc,	"📟 RAM %s%%  ",NULL},
-//	{ netspeed_rx,	"📡 %sB/s  ","wlp2s0" },
-//	{ run_command,	"🔊 %1s  ","amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1" },
-	{ datetime,	"📅 %s  ","%a, %B %d"},
-	{ datetime,	"🕒 %s  ","%I:%M %p"},
-	{ battery_perc,	"☠ %s%%","BAT0"},
-	{ battery_state,"(%s)","BAT0" },
+	/*{	temp, 			"[TEMP %sC] ",	"/sys/class/thermal/thermal_zone0/temp"	},*/
+	/*{	ram_used,		"  %s",	NULL			},*/
+	/*{	datetime, 		" %s ",	"%a %b %d %r"		},*/
+	/*{	cpu_perc, 		" 🗄️ CPU %s%%  ",	NULL		},*/
+	/*{	ram_perc,		"📟 RAM %s%%  ",	NULL		},*/
+	/*{	netspeed_rx,		"📡 %sB/s  ",		"wlp2s0"	},*/
+	/*{	run_command,		"🔊 %1s  ",		"amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1" },*/
+	{	datetime,		"📅 %s  ",		"%a, %B %d"	},
+	{	datetime,		"🕒 %s  ",		"%I:%M %p"	},
+	{	battery_perc,		"☠ %s%%",		"BAT0"		},
+	{	battery_state,		"(%s)",			"BAT0"		},
 };
-
