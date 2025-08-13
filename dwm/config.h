27a69eba6a585ce91807795c3cabe81dd6d17b09
amritxyz <amrit44404@proton.me>
2025-08-13 22:31:45 +0545

    feat: just using more brains cells

--- dwm/config.h
+++ dwm/config.h
@@ -208,7 +208,7 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,		XK_n,		spawn,		SHCMD(TERMINAL " -e newsboat") },
 	{ ALTKEY|ShiftMask,		XK_n,		spawn,		SHCMD("~/.local/bin/notes.sh") },
 	{ MODKEY,			XK_o,		spawn,		SHCMD("xdotool type --delay 0 $(grep -v '^#' ~/.local/share/snippets | dmenu -i -l 10 -p Snippets | cut -d' ' -f1)") },
-	{ MODKEY|ShiftMask,		XK_o,		spawn,		SHCMD("xdotool type --delay 0 $(grep -v '^#' ~/.local/share/key | dmenu -i -l 3 -p Key | cut -d' ' -f1)") },
+	{ MODKEY|ShiftMask,		XK_o,		spawn,		SHCMD("xdotool type --delay 0 $(label=$(grep -v '^#' ~/.local/share/key | awk '{print $2}' | dmenu -i -l 3 -p Key); grep -v '^#' ~/.local/share/key | awk -v pw=\"$label\" '$2 == pw {print $1; exit}')") },
 	/*{ MODKEY|ShiftMask,		XK_s,		spawn,		SHCMD("pkill -USR1 -x sxhkd") },*/
 	{ MODKEY|ShiftMask,		XK_f,		spawn,		SHCMD("~/.local/bin/scrot.sh")},
 	{ MODKEY|ShiftMask,		XK_x,		spawn,		SHCMD("~/.local/bin/setxkbmap.sh")},
