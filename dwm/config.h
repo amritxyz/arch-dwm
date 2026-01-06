b2d12b0fff63a373091a39b72f38e88ef9b89180
amritxyz <amrit44404@proton.me>
2026-01-06 20:57:02 +0545

    added patch to support systray with dwm.

--- dwm/config.h
+++ dwm/config.h
@@ -9,6 +9,11 @@
 /* appearance */
 static unsigned int borderpx	= 3;	// border pixel of windows
 static unsigned int snap	= 32;	// snap pixel
+static const unsigned int systraypinning = 0;   // 0: sloppy systray follows selected monitor, >0: pin systray to monitor X
+static const unsigned int systrayonleft = 1;    // 0: systray in the right corner, >0: systray on left of status text
+static const unsigned int systrayspacing = 2;   // systray spacing
+static const int systraypinningfailfirst = 1;   // 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor
+static const int showsystray        = 1;        // 0 means no systray
 static unsigned int gappih	= 20;	// horiz inner gap between windows
 static unsigned int gappiv	= 10;	// vert inner gap between windows
 static unsigned int gappoh	= 10;	// horiz outer gap between windows and screen edge
@@ -18,7 +23,7 @@ static int smartgaps		= 0;	// 1 means no outer gap when there is only one window
 static int showbar		= 1;	// 0 means no bar
 static int topbar		= 1;	// 0 means bottom bar
 static const int focusonwheel       = 0;
-static char *fonts[]		= { "monospace:size=9:antialias=true:autohint=true", "NotoColorEmoji:pixelsize=12:antialias=true:autohint=true"  };
+static char *fonts[]		= { "monospace:size=9:antialias=true:autohint=true", "NotoColorEmoji:pixelsize=7:antialias=true:autohint=true"  };
 static char normbgcolor[]	= "#222222";
 static char normbordercolor[]	= "#444444";
 static char normfgcolor[]	= "#bbbbbb";
