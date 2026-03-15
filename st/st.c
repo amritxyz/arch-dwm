b0ea5e3467cffc7a4b0553667679f615086dd790
amritxyz <amrit44404@proton.me>
2026-03-15 19:33:27 +0545

    st: fix ATTR_WIDE check using == instead of &
    The code was checking gp[1].mode == ATTR_WIDE to see if the next
    cell is a wide char dummy. This only works if mode has nothing else
    in it. If the wide char also had bold or a color on it, mode would
    be something like ATTR_WIDE|ATTR_BOLD and the == check misses it
    completely and skips the cleanup. Then that dummy cell stays around
    and causes trouble.
    
    Just use & to check the bit. That is what it is there for.

--- st/st.c
+++ st/st.c
@@ -2664,8 +2664,10 @@ check_control_code:
 		gp = &term.line[term.c.y][term.c.x];
 	}
 
-	if (IS_SET(MODE_INSERT) && term.c.x+width < term.col)
+	if (IS_SET(MODE_INSERT) && term.c.x+width < term.col) {
 		memmove(gp+width, gp, (term.col - term.c.x - width) * sizeof(Glyph));
+		gp->mode &= ~ATTR_WIDE;
+	}
 
 	if (term.c.x+width > term.col) {
 		tnewline(1);
@@ -2678,7 +2680,7 @@ check_control_code:
 	if (width == 2) {
 		gp->mode |= ATTR_WIDE;
 		if (term.c.x+1 < term.col) {
-			if (gp[1].mode == ATTR_WIDE && term.c.x+2 < term.col) {
+			if (gp[1].mode & ATTR_WIDE && term.c.x+2 < term.col) {
 				gp[2].u = ' ';
 				gp[2].mode &= ~ATTR_WDUMMY;
 			}
